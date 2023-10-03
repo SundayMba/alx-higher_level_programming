034ba2197c3d2fe8ef14637042f608cde2e19809
SundayMba <sundinoh@gmail.com>
2023-10-03 18:24:19 +0100

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = (number * -1) % 10 if number < 0 else number % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
