01a48a5f1acda4dc5e15a760a711372cf8ab1896
SundayMba <sundinoh@gmail.com>
2023-10-03 13:20:04 +0100

    used tortoise and hare algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,8 +9,21 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *head;
+	listint_t *slow, *fast;
 
+
+	slow = list;
+	fast = list;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+	/*
+	 *
 	if (list == NULL)
 		return (0);
 	head = list;
@@ -20,5 +33,5 @@ int check_cycle(listint_t *list)
 		return (0);
 	if (list->next == head)
 		return (1);
-	return (0);
+		*/
 }
