66416e6f10859bec7a06ef86373df57800742890
SundayMba <sundinoh@gmail.com>
2023-10-03 19:04:43 +0100

    print fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif n % 3 == 0:
+            print("Fizz", end=" ")
+        elif n % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(n, end=" ")
