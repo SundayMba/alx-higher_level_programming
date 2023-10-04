ab14fb908c528de3ab77887a3763d209cbaa617f
SundayMba <sundinoh@gmail.com>
2023-10-04 08:08:50 +0100

    advanced project

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 
 def remove_char_at(str, n):
     new = []
-    for i in  range(0, len(str)):
+    for i in range(0, len(str)):
         if i != n:
             new.append(str[i])
     return "".join(new)
