3fae32912cf63c43cc3f206de83c6037706e6b9b
SundayMba <sundinoh@gmail.com>
2023-10-09 23:18:35 +0100

    matrix printing

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        count = 0
+        for item in row:
+            print("{:d}".format(item), end="")
+            count += 1
+            if count != len(row):
+                print(end=" ")
+        print("")
