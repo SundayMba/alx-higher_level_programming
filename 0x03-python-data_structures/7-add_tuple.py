190428e8ea2bab8660657137e51148b41bd7b62b
SundayMba <sundinoh@gmail.com>
2023-10-10 10:23:13 +0100

    removed error in name

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,7 +12,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) > 2:
         a1, a2 = tuple_a[0], tuple_a[1]
     if len(tuple_b) > 2:
-        b1, b2 = tuplee_b[0], tuple_b[1]
+        b1, b2 = tuple_b[0], tuple_b[1]
     elif len(tuple_b) < 2:
         if len(tuple_b) == 1:
             b1, b2 = tuple_b[0], 0
