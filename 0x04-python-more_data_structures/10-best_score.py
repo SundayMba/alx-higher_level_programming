d919cd6abe4828b4cf39b08e172e21bff5ce7be5
SundayMba <sundinoh@gmail.com>
2023-10-11 14:25:35 +0100

    added for empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     name = ''
     max_score = -999999999999999999999  # an arbitrary selected number
