f967903db47647d24ce63f0d575efec16823959d
SundayMba <sundinoh@gmail.com>
2023-10-23 23:17:00 +0100

    used string format method

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    idx = 0
     try:
-        idx = 0
         while idx < x:
-            print(my_list[idx], end="")
+            print("{}".format(my_list[idx]), end="")
             idx += 1
         print()
         return idx
