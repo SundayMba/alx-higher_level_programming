58be5c526f7897b2d686d78dcf46c10867cc9a28
SundayMba <sundinoh@gmail.com>
2023-10-25 11:58:32 +0100

    implemented one private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Square module: this class <Square> models a Square object"""
+
+
+class Square:
+    """ square class
+
+    this class is a minimal square class with no implementation
+    just pass.
+
+    """
+    def __init__(self, size):
+        """ __init__ method
+
+        this method is a special method that acts as an instance
+        constructor after instantiation
+
+        Args:
+            size (int): <size> is a private attribute of type integer.
+
+        """
+        self.__size = size
