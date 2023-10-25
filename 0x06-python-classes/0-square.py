9f47da598b3d2639b79533e153e31bdabfc462d2
SundayMba <sundinoh@gmail.com>
2023-10-25 11:41:42 +0100

    0-square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Square module: this class <Square> models a Square object"""
+class Square:
+    """square class: 
+    this class is a minimal square class with no implementation
+    just pass.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+### 0x06-python-classes
+    0. 0 - square
