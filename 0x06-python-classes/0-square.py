a3f99c06867b4f8a84a5070fdb2e1f995d864e90
SundayMba <sundinoh@gmail.com>
2023-10-25 11:45:55 +0100

    removed whitespace

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 """Square module: this class <Square> models a Square object"""
+
+
 class Square:
-    """square class: 
+    """ square class
+
     this class is a minimal square class with no implementation
     just pass.
+
     """
     pass
