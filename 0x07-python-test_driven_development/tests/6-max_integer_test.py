5c85d30e52f9f806758ac178ba1a331e7e616f7f
SundayMba <sundinoh@gmail.com>
2023-11-10 22:27:17 +0100

    added unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,4 +13,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         self.assertEqual(max_integer([2, 9, 0, -4]), 9)
         self.assertIsNone(max_integer([]))
-        self.assertEqual(max_integer([3, 3, 9, 9, -1]), 9)
+        self.assertEqual(max_integer([3, 3, 9, 9]), 9)
+        self.assertEqual(max_integer([10, 3, 9, 9]), 10)
+        self.assertEqual(max_integer([3, 3, 12, 9, 0]), 12)
+        self.assertEqual(max_integer([-2, -2, -2, -3, -5, -6]), -2)
+        self.assertEqual(max_integer([3]), 3)
