d718d2a1c6b2992239ecbca57635dc04d5c2a193
SundayMba <sundinoh@gmail.com>
2023-11-13 19:29:29 +0100

    modified documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from models.base import Base
 """ Subclass module for Base implementing Rectangle """
 
+from models.base import Base
 
 class Rectangle(Base):
     """ Subclass for Base """
