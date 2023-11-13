bdafc78e3be370adfc31f514126795f64dc3a97a
SundayMba <sundinoh@gmail.com>
2023-11-13 19:59:51 +0100

    added check for empty string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,6 @@ class Base:
         """ convert from list of dictionaries to json string """
         if list_dictionaries is None:
             return '"[]"'
-        if list_dictionaries == []:
-            return "\"[]\""
         return str(list_dictionaries).replace("'", '"')
 
     @classmethod
