622d3b3e74a12816996394a333e9e2a272757f6f
sundaymba <sundinoh@gmail.com>
2023-12-13 14:33:16 +0000

    changed table set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,16 +5,5 @@
 USE hbtn_0c_0
 
 -- CONVERT DATABASE TO utf8mb4
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- CONVERT TABLE TO utf8mb4
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- CONVERT COLUMN TO utf8mb4
-ALTER TABLE first_table
-MODIFY name
-VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
