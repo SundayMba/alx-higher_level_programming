3e2e8ac8ebd4f1c56b9bcecef342fcf2d5510c5a
sundaymba <sundinoh@gmail.com>
2023-12-13 14:12:17 +0000

    handled advanced functions

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -14,5 +14,4 @@ COLLATE utf8mb4_unicode_ci;
 -- CONVERT COLUMN TO utf8mb4
 ALTER TABLE first_table
 MODIFY name
-VARCHAR(256) CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+VARCHAR(256) COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,9 @@
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+-- import dump file
+source temperatures.sql
+
+-- perform operation
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
