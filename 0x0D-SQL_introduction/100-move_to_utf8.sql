469dd0fa927dcb526fb2bf51df7c6c778faca288
sundaymba <sundinoh@gmail.com>
2023-12-13 12:54:52 +0000

    changed charset and show full details

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,18 @@
+-- script that converts hbtn_0c_0 database to UTF8
+-- (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+
+-- CONVERT DATABASE TO utf8mb4
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- CONVERT TABLE TO utf8mb4
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- CONVERT COLUMN TO utf8mb4
+ALTER TABLE first_table
+MODIFY name
+VARCHAR(256) CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server.
 -- show full table detail
-SELECT * FROM first_table;
+SHOW CREATE TABLE first_table;
