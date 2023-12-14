d11f72d0a4703af88723c46f9850f16940e8cb24
sundaymba <sundinoh@gmail.com>
2023-12-14 08:25:03 +0000

    changed password

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create a user for hbtn_0d_2 database
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'User_0d_2_pwd';
+IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant only select privilege
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
