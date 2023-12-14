11de1b1ad7a87d878ca666310c69d2ae0463b1f0
sundaymba <sundinoh@gmail.com>
2023-12-14 10:43:24 +0000

    used sub queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- script that lists all the cities of California
+-- that can be found in the database hbtn_0d_usa.
+
+-- USE subqueries instead of JOIN
+SELECT * FROM cities
+WHERE state_id = (
+		SELECT id FROM states
+		WHERE name = 'California')
+ORDER BY id ASC;
+
