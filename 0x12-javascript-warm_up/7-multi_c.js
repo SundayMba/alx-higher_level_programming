35bc2cea325b65a6ac056b12668cfede795b74d2
sundaymba <sundinoh@gmail.com>
2024-01-08 21:10:27 +0000

    used for loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   x = Math.floor(Number(x));
-  while (x) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
-    x--;
   }
 }
+
