6f5502d18dff9be29751210b7eb9f4e07c7ea1c9
SundayMba <sundinoh@gmail.com>
2024-01-11 14:56:18 +0100

    removed try/finally block

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,33 +3,35 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    try:
-        # connection parameters
-        args = sys.argv
-        user = args[1]
-        password = args[2]
-        database = args[3]
-        host = 'localhost'
-        port = 3306
+    # connection parameters
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+    host = 'localhost'
+    port = 3306
 
-        # database connection
-        conn = MySQLdb.connect(
-                host=host,
-                port=port,
-                user=user,
-                passwd=password,
-                db=database
-                )
-        # grab the cursor to access the database
-        cursor = conn.cursor()
-        query = f'SELECT * FROM states ORDER BY states.id'
+    # database connection
+    conn = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=password,
+        db=database
+        )
+    # grab the cursor to access the database
+    cursor = conn.cursor()
+    query = f'SELECT * FROM `states` ORDER BY states.id'
 
-        # query executation
-        cursor.execute(query)
+    # query executation
+    cursor.execute(query)
 
-        # access the fetched data
-        rows = cursor.fetchall()
+    # access the fetched data
+    rows = cursor.fetchall()
 
-        # DISPLAY RESULT
-        for row in rows:
-            print(row)
+    # DISPLAY RESULT
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
