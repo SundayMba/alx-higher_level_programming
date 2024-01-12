1baa745b4bce7564e15000236fd2f3cdada62d04
SundayMba <sundinoh@gmail.com>
2024-01-12 08:30:36 +0100

    filter state given name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    module for state filter given name
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # connection parameters
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+    search = args[4]
+    host = 'localhost'
+    port = 3306
+
+    # database connection
+    conn = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=password,
+        db=database
+        )
+    # grab the cursor to access the database
+    cursor = conn.cursor()
+    query = 'SELECT * FROM `states` WHERE states.name=={}'.format(search)
+
+    # query executation
+    cursor.execute(query)
+
+    # access the fetched data
+    rows = cursor.fetchall()
+
+    # DISPLAY RESULT
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
