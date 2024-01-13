c068ee0bec20fce537862b89bf292f5347ed0b2f
SundayMba <sundinoh@gmail.com>
2024-01-13 01:25:25 +0100

    search cities and states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    module for state filter given name
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # connection parameters
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+    host = 'localhost'
+    port = 3306
+
+    # database connection
+    conn = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=password,
+        db=database
+        )
+    # grab the cursor to access the database
+    cursor = conn.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM `cities` \
+    INNER JOIN states ON states.id = cities.state_id \
+    ORDER BY states.id"
+
+    # query executation
+    cursor.execute(query)
+
+    # access the fetched data
+    rows = cursor.fetchall()
+
+    # DISPLAY RESULT
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
