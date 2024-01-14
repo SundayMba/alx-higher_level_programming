55f9c34f2a39706c7db1933bcf9217cd5ea482f5
SundayMba <sundinoh@gmail.com>
2024-01-15 00:34:49 +0100

    using backref to access state from city

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    module to list all state object from the database
+"""
+
+from relationship_state import State
+from relationship_city import City, Base
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    # connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # create the table
+    Base.metadata.create_all(engine)
+    # create a session with the connected database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # using the session, communicate with the database
+    cities = session.query(City)
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
