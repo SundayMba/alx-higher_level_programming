0b5a7014a45793a3082f5d9cc167bdb4bfaed525
SundayMba <sundinoh@gmail.com>
2024-01-14 23:23:43 +0100

    added relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    module that models city object
+"""
+from model_state import Base
+from sqlalchemy import Integer, Column, String, ForeignKey
+
+
+class City(Base):
+    """
+        class that models city
+        id: sqlalchemy.integer, primary key, autoincrement, not null
+        name: sqlalchemy.String, String(128), not null
+        state_id: foreign_key, not null, Integer
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
