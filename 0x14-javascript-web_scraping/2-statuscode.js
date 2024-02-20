cb2c4f5062b4f0752370acf0641cf42aaed93a67
SundayMba <sundinoh@gmail.com>
2024-02-20 15:51:48 +0100

    statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (error, res) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
