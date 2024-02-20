66ad5d473d4666363ebf70c985db8e1f413ad747
SundayMba <sundinoh@gmail.com>
2024-02-20 16:36:16 +0100

    fetched starwars

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const request = require('request');
+const url = process.argv[2];
+
+let count = 0;
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movies = JSON.parse(body);
+    movies.results.forEach(movie => {
+      movie.characters.forEach(c => {
+        if (c.includes('18')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
