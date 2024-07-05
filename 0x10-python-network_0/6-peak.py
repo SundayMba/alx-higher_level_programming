e73feaa34f6e231b92b7ada814692685741510e4
SundayMba <sundinoh@gmail.com>
2024-07-05 01:08:17 +0100

    use for loop

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
-
+"""
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
+    #Return a peak in a list of unsorted integers.
     if list_of_integers == []:
         return None
 
@@ -21,3 +21,26 @@ def find_peak(list_of_integers):
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
+"""
+def find_peak(list_of_integers):
+    peaks = []
+    if list_of_integers == []:
+        return None
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+    else:
+        for i, n in enumerate(list_of_integers):
+            if i == 0 and n >= list_of_integers[i+1]:
+                peaks.append(n)
+            elif i == size - 1 and n > list_of_integers[i-1]:
+                peaks.append(n)
+            elif i != 0 and i != size - 1:
+                if n >= list_of_integers[i+1] and n >= list_of_integers[i-1]:
+                    peaks.append(n)
+        if len(peaks) > 1:
+            return max(peaks)
+        else:
+            return peaks[0]
